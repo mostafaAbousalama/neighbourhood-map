0ac0aad47a129179cc400a0d11fe6594e70c176e
Mostafa Adel Abou-Salama <abousalama.mostafa@gmail.com>
2018-08-27 16:56:49 +0200

    Foursquare search and state management of places functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var foursquare = require('react-foursquare')({
+  clientID: '5NDJGWHYZ4AGS4PMS4532RXMHJRB322SBFJCKM5KYGAIYCUI',
+  clientSecret: 'FZMDM5LEDXCDTG1AX2KB35GE3CZ2NMOKWD304Y125QGQORMI'
+});
+
+var params = {
+  "near": "Alexandria, Egypt",
+  "categoryId": "4d4b7104d754a06370d81259"  //  Limit search matches to Arts & Entertainment venues
+};
+
 class App extends Component {
+
+  state = {
+    places: [],
+    errorMessage: ""
+  };
+
+  componentDidMount() {
+    foursquare.venues.getVenues(params)
+      .then(res=> {
+        this.setState({ places: res.response.venues });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Neighbourhood Map App using React, Google Maps API and Foursquare API's React Library</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.errorMessage ? (<div>{this.state.errorMessage}</div>) : (<div></div>)
+        }
       </div>
     );
   }
